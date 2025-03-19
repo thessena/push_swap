a9e81b559cf6827bbb784c18a34f10a523bb09f8
thessena <timhessenauer@gmail.com>
2025-03-19 11:38:35 +0100

    wrote function for when main-stack has only three nodes left

--- src/smart_sort.c
+++ src/smart_sort.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 10:46:53 by thessena          #+#    #+#             */
-/*   Updated: 2025/03/19 11:21:50 by thessena         ###   ########.fr       */
+/*   Updated: 2025/03/19 11:37:44 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,30 @@ int	get_list_size(t_stack *list)
 	return (size);
 }
 
+t_stack	*find_highest(t_stack **list)
+{
+	t_stack	*highest = *list;
+	t_stack	*current = *list;
+	while (current)
+	{
+		if (current->value > highest->value)
+			highest = current;
+		current = current->next;
+	}
+	return (highest);
+}
+
+void	order_three(t_stack **list)
+{
+	t_stack *top_value = find_highest(list);
+	if (top_value->index == 0)
+		ra(list);
+	else if (top_value->index == 1)
+		rra(list);
+	if (!is_in_order(list))
+		sa(list);
+}
+
 void	smart_sort(t_stack **main, t_stack **helper)
 {
 	int		i;
@@ -57,5 +81,23 @@ void	smart_sort(t_stack **main, t_stack **helper)
 	}
 	pb(main, helper);
 	pb(main, helper);
-
+	while (get_list_size(*main) > 3)
+	{
+		i = 0;
+		current = *main;
+		while (current)
+		{
+			current->index = i++;
+			current = current->next;
+		}
+		i = 0;
+		current = *helper;
+		while (current)
+		{
+			current->index = i++;
+			current = current->next;
+		}
+		pb(main, helper);
+	}
+	order_three(main);
 }
