a7b0c8922f7ce1261c83f5dec1b95937a837b6f8
thessena <timhessenauer@gmail.com>
2025-03-21 17:48:46 +0100

    implementing first functions

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 11:26:38 by thessena          #+#    #+#             */
-/*   Updated: 2025/03/21 17:32:56 by thessena         ###   ########.fr       */
+/*   Updated: 2025/03/21 17:44:37 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,17 +65,11 @@ int	main(int argc, char **argv)
 	}
 	a = init_stack(argc, argv);
 	b = NULL;
-/* 	if (!a)
-	{
-		write(1, "Error\n", 6);
-		return (1);
-	} */
 	write(1, "Vor Sortierung:\n", 16);
 	print_stack(a);
 	turk_sort(&a, &b);
 	write(1, "Nach Sortierung:\n", 17);
 	print_stack(a);
-	// print_stack(b);
 	free_stack(a);
 	free_stack(b);
 	return (0);
--- src/turk_sort.c
+++ src/turk_sort.c
@@ -6,10 +6,60 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 16:25:51 by thessena          #+#    #+#             */
-/*   Updated: 2025/03/21 17:43:06 by thessena         ###   ########.fr       */
+/*   Updated: 2025/03/21 17:48:27 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+int	get_list_size(t_stack *list)
+{
+	int	size;
 
+	size = 0;
+	while (list)
+	{
+		size++;
+		list = list->next;
+	}
+	return (size);
+}
+
+int	is_sorted(t_stack *list)
+{
+	while (list && list->next)
+	{
+		if (list->value > list->next->value)
+			return (0);
+		list = list->next;
+	}
+	return (1);
+}
+
+t_stack	*get_max_node(t_stack *list)
+{
+	t_stack	*max;
+
+	max = list;
+	while (list)
+	{
+		if (list->value > max->value)
+			max = list;
+		list = list->next;
+	}
+	return (max);
+}
+
+t_stack	*get_min_node(t_stack *list)
+{
+	t_stack	*min;
+
+	min = list;
+	while (list)
+	{
+		if (list->value < min->value)
+			min = list;
+		list = list->next;
+	}
+	return (min);
+}
